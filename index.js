051f1bc96e4bbca751afeec876a1f1ad7dfb0c46
Nanzi Mustafa Chowdhury <ge42poj@mytum.de>
2022-11-09 23:33:48 +0600

    services api done

--- index.js
+++ index.js
@@ -12,12 +12,23 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mtryllz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run () {
+    try{
+        const serviceCollection = client.db('photoStudio').collection('services');
+
+        app.get('/services', async(req,res) =>{
+            const query ={}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(e => console.log(e));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
